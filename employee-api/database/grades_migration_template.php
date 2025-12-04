480d497d6479eb8c84a6d92963238831a6f7dc0e
Sam Lunnemann <132962624+interestingpoint@users.noreply.github.com>
2025-12-03 19:57:23 -0500

    Add files via upload

---
+++ employee-api/database/grades_migration_template.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('english grade');
+            $table->string('math grade');
+            $table->string('ss grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
---
+++ employee-api/database/migrations/2025_11_29_211742_create_tickers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickers', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->string('full name');
+            $table->integer('is_up');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickers');
+    }
+};
